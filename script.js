48d738fd2b9e22bae0cd4dee0b4db3800ac138f6
idasoderlund <soderlundida7711@gmail.com>
2025-03-27 16:40:13 +0100

    added togglebutton js

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .catch((error) => console.error("Error fetching data", error));
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("togglefunction");
+
+  toggleButton.addEventListener("click", function () {
+    if (toggleButton.textContent === "Hide information") {
+      toggleButton.textContent = "Show information";
+      toggleButton.style.backgroundColor = "#C1D8C3";
+    } else {
+      toggleButton.textContent = "Hide information";
+      toggleButton.style.backgroundColor = "#E50046";
+    }
+  });
+});
