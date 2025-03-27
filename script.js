f556ce8a740d09847351264ebe833a3f4bae69ee
idasoderlund <soderlundida7711@gmail.com>
2025-03-27 16:27:37 +0100

    added js and removed h3 from html

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-function getAllUsers() {
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response is out of function");
-      }
-      return response.json();
-    })
-    .then((users) => {
-      const usersContainer = document.getElementById("users");
-      usersContainer.innerHTML = "";
+document.addEventListener("DOMContentLoaded", function () {
+  const usersContainer = document.getElementById("users");
 
-      users.forEach((users) => {
-        let content = `<div id = "userCard">
-        <h3>${user.name}</h3>
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => {
+      users.forEach((user) => {
+        let content = `
+      <div class="userCard">
+      <h3>${user.name}</h3>
         <h4>${user.username}</h4>
         <h3>${user.email}</h3>
         </div>`;
         usersContainer.innerHTML += content;
       });
     })
-    .catch((e) => {
-      alert("error" + e);
-    });
-}
-
-getAllUsers();
+    .catch((error) => console.error("Error fetching data", error));
+});
